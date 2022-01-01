83a98bb0f26831bd5828cfc9a106d80fad099867
SaranSundar <saransszg@gmail.com>
2021-12-31 23:23:52 -0600

    Changed int enum to str

--- enums/EndTurnAction.py
+++ enums/EndTurnAction.py
@@ -1,14 +1,14 @@
-from enum import IntEnum
+from enum import Enum
 
 
-class EndTurnAction(IntEnum):
+class EndTurnAction(str, Enum):
     # Any action can get a noble if player chooses to at that turn
-    BuyingCard = 1
-    BuyingGoldToken = 2
-    Buying3DifferentTokens = 3
-    Buying2SameTokens = 4
-    # When you can only buy 1 or 2 tokens cause you have 8 or 9
-    BuyingLimitedTokens = 5
+    BuyingCard = 'BuyingCard'
+    BuyingGoldToken = 'BuyingGoldToken'
+    Buying3DifferentTokens = 'Buying3DifferentTokens'
+    Buying2SameTokens = 'Buying2SameTokens'
+    # When you can only buy 1 or 2 tokens because you have 8 or 9
+    BuyingLimitedTokens = 'BuyingLimitedTokens'
 
     def __str__(self):
         return self.name
